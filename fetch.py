a65c1e182058745d41a31a41d776ce71eee895e7
piryth <arnaud.endignous@etu.univ-grenoble-alpes.fr>
2024-04-05 14:53:01 +0100

    Updated tests and remove test fetch file

--- fetch.py
+++
@@ -1,8 +0,0 @@
-import json
-
-import requests
-
-if __name__ == '__main__':
-    res = requests.get("http://localhost:5000/titles")
-    data = json.loads(res.content)
-    print(data)
--- test/test.py
+++ test/test.py
@@ -1,28 +1,94 @@
 import json
 import unittest
+import uuid
 
 import requests
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)  # add assertion here
 
+    def test_titles_insert(self):
+        url = "http://localhost:5000/products?secret=loremipsumdolorsitamet"
 
-        def test_titles_insert(self):
-            data = {
-                "id": "1000",
-                "title": "food",
-                "cost": 100
-            }
-            res = requests.post("http://localhost:5000/products", data)
-            data = json.loads(res.content)
-            print(data)
+        payload = json.dumps({
+            "id": uuid.uuid4().__str__(),
+            "title": "food",
+            "cost": 15
+        })
+        headers = {
+            'Content-Type': 'application/json'
+        }
 
-            res = requests.get("http://localhost:5000/titles")
-            data = json.loads(res.content)
-            print(data)
+        response = requests.request("POST", url, headers=headers, data=payload)
 
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.text, "Product inserted")
+
+    def test_titles_insert_no_secret(self):
+        url = "http://localhost:5000/products?secret="
+        id = "myid"
+        payload = json.dumps({
+            "id": id,
+            "title": "test_insert_no_secret",
+            "cost": 15
+        })
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        # First product inserted
+        response = requests.request("POST", url, headers=headers, data=payload)
+        self.assertEqual(response.status_code, 403)
+
+    def test_titles_insert_conflict(self):
+        url = "http://localhost:5000/products?secret=loremipsumdolorsitamet"
+        id = uuid.uuid4().__str__()
+        payload = json.dumps({
+            "id": id,
+            "title": "test_insert_no_secret",
+            "cost": 15
+        })
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        # First product inserted
+        response = requests.request("POST", url, headers=headers, data=payload)
+        self.assertEqual(response.status_code, 200)
+        # Second product inserted with same id
+        response2 = requests.request("POST", url, headers=headers, data=payload)
+        self.assertEqual(response2.status_code, 409)
+
+    def test_get_products(self):
+        url = "http://localhost:5000/products"
+
+        payload = {}
+        headers = {}
+
+        response = requests.request("GET", url, headers=headers, data=payload)
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_products_by_name(self):
+        url = "http://localhost:5000/products/test_name_item"
+
+        payload = {}
+        headers = {}
+
+        response = requests.request("GET", url, headers=headers, data=payload)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.text, "No product corresponding to title test_name_item")
+
+        url2 = "http://localhost:5000/products/food"
+
+        response = requests.request("GET", url, headers=headers, data=payload)
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_titles(self):
+        url = "http://localhost:5000/titles"
+        response = requests.request("GET", url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.text is not None)
 
 if __name__ == '__main__':
     unittest.main()
