424b7c660d59614d4dbdcf360a9f9cfbb0a170ee
piryth <arnaud.endignous@etu.univ-grenoble-alpes.fr>
2024-04-05 11:48:30 +0100

    tests

--- test/test.py
+++ test/test.py
@@ -8,11 +8,6 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)  # add assertion here
 
-    # We test directly the REST endpoint that calls the GraphQL endpoint
-    def test_titles_api(self):
-        res = requests.get("http://localhost:5000/titles")
-        data = json.loads(res.content)
-        print(data)
 
         def test_titles_insert(self):
             data = {
@@ -21,6 +16,10 @@ class MyTestCase(unittest.TestCase):
                 "cost": 100
             }
             res = requests.post("http://localhost:5000/products", data)
+            data = json.loads(res.content)
+            print(data)
+
+            res = requests.get("http://localhost:5000/titles")
             data = json.loads(res.content)
             print(data)
 
